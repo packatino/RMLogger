9d89197727474993ee4ef1fd4319425fc53d2a91
Robert Mißbach <mail@robert-missbach.com>
2015-08-13 16:41:55 +0200

    created the Xcode project

---
+++ RMLogger/RMLogger/RMLogger.h
@@ -0,0 +1,13 @@
+//
+//  RMLogger.h
+//  RMLogger
+//
+//  Created by Robert Mißbach on 13.08.15.
+//  Copyright (c) 2015 Robert Mißbach. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMLogger : NSObject
+
+@end
---
+++ RMLogger/RMLogger/RMLogger.m
@@ -0,0 +1,13 @@
+//
+//  RMLogger.m
+//  RMLogger
+//
+//  Created by Robert Mißbach on 13.08.15.
+//  Copyright (c) 2015 Robert Mißbach. All rights reserved.
+//
+
+#import "RMLogger.h"
+
+@implementation RMLogger
+
+@end
