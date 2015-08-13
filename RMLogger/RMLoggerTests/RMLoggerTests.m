ef2571b58ed860e8ab7c25615aeab837b2859af3
Robert Mißbach <mail@robert-missbach.com>
2015-08-13 16:51:06 +0200

    use cocoapods and install OCMock

---
+++ RMLogger/RMLoggerTests/RMLoggerTests.m
@@ -0,0 +1,21 @@
+//
+//  RMLoggerTests.m
+//  RMLogger
+//
+//  Created by Robert Mißbach on 13.08.15.
+//  Copyright (c) 2015 Robert Mißbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import "RMLogger.h"
+
+@interface RMLoggerTests : XCTestCase
+
+@end
+
+
+@implementation RMLoggerTests
+
+@end
